d915242b4968d715be121264f4666d23b8f64118
ldoyle2020 <61572199+ldoyle2020@users.noreply.github.com>
2023-05-02 07:08:26 +0000

    Fixing up data import

--- Q2/Q2.py
+++ Q2/Q2.py
@@ -1,2 +1,15 @@
-from datasets import load_dataset
-dataset = load_dataset("cardiffnlp/tweet_topic_single")
+import datasets
+import numpy as np
+import torch
+import torch.nn as nn
+import torchtext
+
+#Loading the data set
+train_data, test_data = datasets.load_dataset("cardiffnlp/tweet_topic_single", split=["train_coling2022", "test_coling2022"])
+
+#Allocating 20% for validation
+train_valid_data = train_data.train_test_split(test_size=0.2)
+train_data = train_valid_data['train_coling2022']
+valid_data = train_valid_data['test_coling2022']
+
+train_data, valid_data, test_data
